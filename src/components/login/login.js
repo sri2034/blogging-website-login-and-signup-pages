914c13e7620492d24438f2edbed72a896ef5333e
sri2034 <pavansriramkurukuri3@gmail.com>
2023-11-06 23:06:43 +0530

    first commit

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,43 +1,78 @@
-import React, { useState } from 'react'
-import './login.css'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import './login.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-const Login = ({ setLoginUser}) => {
-
+const Login = ({ setLoginUser }) => {
   const history = useNavigate();
 
-  const [user,setUser] = useState({
-    email: "",
-    password: ""
+  const [user, setUser] = useState({
+    username: '',
+    password: '',
   });
 
-  const handleChange = e => {
-      const {name, value} = e.target
-      setUser({
-          ...user,
-          [name]: value
-      })
-  }
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
 
   const login = () => {
-    axios.post("https://my-new-5a71.onrender.com/login", user).then(res => {
-      alert(res.data.message)
-      setLoginUser(res.data.user)
-      history("/")
-  })
-  }
+    if (user.username.trim() === '' || user.password.trim() === '') {
+      setErrorMessage('Please fill in all fields.');
+    } else {
+      axios
+        .post('https://my-new-5a71.onrender.com/login', user)
+        .then((res) => {
+          setErrorMessage('');
+          setLoginUser(res.data.user);
+          history('/');
+        })
+        .catch((error) => {
+          if (error.response) {
+            setErrorMessage(error.response.data.message);
+          } else {
+            setErrorMessage('An error occurred during login.');
+            console.error(error);
+          }
+        });
+    }
+  };
 
   return (
-    <div className="login">
+    <div className="App">
+      <div className="login">
         <h1>Login</h1>
-        <input type="text" name='email' value={user.email} placeholder='Enter your Email' onChange={ handleChange } />
-        <input type="password" name='password' value={user.password} placeholder='Enter your password' onChange={ handleChange } />
-        <div className="button" onClick={login}>Login</div>
+        <input
+          type="text"
+          name="username"
+          value={user.username}
+          placeholder="Enter your Username"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={user.password}
+          placeholder="Enter your password"
+          onChange={handleChange}
+        />
+        {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
+        <div className="button" onClick={login}>
+          Login
+        </div>
         <div>or</div>
-        <div className="button" onClick={() => history("/register")}>Register</div>
+        <Link className="li-reg button" to="/register">
+          Register
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,52 +1,166 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import logo from '../.././logo.svg';
+import { Link } from 'react-router-dom';
 import axios from 'axios'
-import './register.css'
+import './register.css';
 import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
-
     const history = useNavigate();
 
-    const [user,setUser] = useState({
-        name:"",
+    const [user, setUser] = useState({
+        name: "",
+        username: "",
         email: "",
+        phno: "",
         password: "",
-        reEnterPassword: ""
+        reEnterPassword: "",
+        dob: ""
     });
 
-    const handleChange = e => {
-        const {name, value} = e.target
+    const [error, setError] = useState("");
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
         setUser({
             ...user,
             [name]: value
-        })
-    }
+        });
+    };
+
+    // const register = () => {
+    //     const { name, username, email, phno, password, reEnterPassword, dob } = user;
+    //     if (
+    //         name &&
+    //         username &&
+    //         email &&
+    //         phno &&
+    //         password &&
+    //         reEnterPassword &&
+    //         dob
+    //     ) {
+    //         if (password === reEnterPassword) {
+    //             // Additional form validation can be added here (e.g., email format, phone number format).
+    //             axios
+    //                 .post("http://localhost:4000/register", user)
+    //                 .then((res) => {
+    //                     alert(res.data.message);
+    //                     history("/login");
+    //                 })
+    //                 .catch((error) => {
+    //                     setError("Registration failed. Please try again later.");
+    //                 });
+    //         } else {
+    //             setError("Passwords do not match.");
+    //         }
+    //     } else {
+    //         setError("Please fill in all required fields.");
+    //     }
+    // };
 
     const register = () => {
-        const {name,email,password,reEnterPassword} = user
-        if(name && email && password && (password === reEnterPassword)) {
-            axios.post("https://my-new-5a71.onrender.com/register", user).then( res => {
-                alert(res.data.message)
-                history("/login")
-        })
-        } else {
-            alert("invalid input")
-        }
+        const { name, username, email, phno, password, reEnterPassword, dob } = user;
         
-    }
+        if (
+          name &&
+          username &&
+          email &&
+          phno &&
+          password &&
+          reEnterPassword &&
+          dob
+        ) {
+          if (password === reEnterPassword) {
+            // Additional form validation can be added here (e.g., email format, phone number format).
+            axios
+              .post("https://my-new-5a71.onrender.com/register", user)
+              .then((res) => {
+                alert(res.data.message);
+                history("/login");
+              })
+              .catch((error) => {
+                // Handle server-side errors here
+                if (error.response) {
+                  setError(error.response.data.message);
+                } else {
+                  setError("Registration failed. Please try again later.");
+                }
+              });
+          } else {
+            setError("Passwords do not match.");
+          }
+        } else {
+          setError("Please fill in all required fields.");
+        }
+      };
+      
 
-  return (
-    <div className="register">
-        <h1>Register</h1>
-        <input type="name" name='name' value={user.name} placeholder='Enter your Name' onChange={ handleChange } />
-        <input type="text" name='email' value={user.email} placeholder='Enter your Email' onChange={ handleChange } />
-        <input type="password" name='password' value={user.password} placeholder='Enter your password' onChange={ handleChange } />
-        <input type="password" name='reEnterPassword' value={user.reEnterPassword} placeholder='Re-enter your password' onChange={ handleChange } />
-        <div className="button" onClick={register}>Register</div>
-        <div>or</div>
-        <div className="button" onClick={() => history("/login")}>Login</div>
-    </div>
-  )
-}
+    return (
+        <div className='registration-container row' style={{ borderRadius: '10px' }}>
+            <div className='logo col-lg-6 col-md-6 col-sm-12'><img style={{ borderRadius: '10px' }} src={logo} alt="logo" required /></div>
+            <div className='reg-form col-lg-6 col-md-6 col-sm-12'>
+                <form className='form' onSubmit={(e) => {
+                    e.preventDefault(); // Prevent the default form submission behavior
+                    register();
+                }}>
+                    <table className='mx-auto' style={{ borderRadius: '10px' }}>
+                        <thead>
+                            <tr>
+                                <th colSpan={2}>
+                                    <h3 className='text-center'>Register Yourself</h3>
+                                    <p className='text-center'>& share what you write</p>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className='p-1'><label className='form-label'>Name</label></td>
+                                <td className='p-1'><input style={{ borderWidth: '3px' }} className='form-control rounded-1 ms-4' type="text" name='name' value={user.name} onChange={handleChange} required /></td>
+                            </tr>
+                            <tr>
+                                <td className='p-1'><label className='form-label'>Username</label></td>
+                                <td className='p-1'><input style={{ borderWidth: '3px' }} className='form-control rounded-1 ms-4' type="text" name='username' value={user.username} onChange={handleChange} required /></td>
+                            </tr>
+                            <tr>
+                                <td className='p-1'><label className='form-label'>Email</label></td>
+                                <td className='p-1'><input style={{ borderWidth: '3px' }} className='form-control rounded-1 ms-4' type="email" name='email' value={user.email} onChange={handleChange} required /></td>
+                            </tr>
+                            <tr>
+                                <td className='p-1'><label className='form-label'>Phone Number</label></td>
+                                <td className='p-1'><input style={{ borderWidth: '3px' }} className='form-control rounded-1 ms-4' type="tel" name='phno' value={user.phno} onChange={handleChange} required /></td>
+                            </tr>
+                            <tr>
+                                <td className='p-1'><label className='form-label'>Password</label></td>
+                                <td className='p-1'><input style={{ borderWidth: '3px' }} className='form-control rounded-1 ms-4' type="password" name='password' value={user.password} onChange={handleChange} required /></td>
+                            </tr>
+                            <tr>
+                                <td className='p-1'><label className='form-label'>Confirm Password</label></td>
+                                <td className='p-1'><input style={{ borderWidth: '3px' }} className='form-control rounded-1 ms-4' type="password" name='reEnterPassword' value={user.reEnterPassword} onChange={handleChange} required /></td>
+                            </tr>
+                            <tr>
+                                <td className='p-1'><label className='form-label'>Date of birth</label></td>
+                                <td className='p-1'><input style={{ borderWidth: '3px' }} className='form-control rounded-1 ms-4' type="date" name='dob' value={user.dob} onChange={handleChange} required /></td>
+                            </tr>
+                            <tr>
+                                <td className="pt-4 pb-2 text-center" colSpan={2}>
+                                    {error && <span className="alert alert-danger">{error}</span>}
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td className='text-center pt-3' colSpan={2}>
+                                    <Link className='ms-auto' to='/Login'><button className='btn btn-primary opacity-75'>Login</button></Link>
+                                    <button onClick={register} type="submit" className='mx-3 btn btn-info opacity-75'>Register</button>
+                                </td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </form>
+            </div>
+        </div>
+    );
 
-export default Register
+};
+
+export default Register;
